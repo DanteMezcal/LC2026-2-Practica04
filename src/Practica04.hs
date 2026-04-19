53fc0f89c3fb645d2dd2593427f110c72557c919
DanteMezcal <322037461@ciencias.unam.mx>
2026-04-19 01:08:20 -0600

    Funciones auxiliares

--- src/Practica04.hs
+++ src/Practica04.hs
@@ -43,7 +43,12 @@ success = undefined
 
 --Ejercicio 3
 unit :: Estado -> Estado
-unit = undefined
+unit (modelo , []) = (modelo , [])
+unit (modelo , c:xs) = if esUnitaria c
+                          then if tieneInterpretacion (obtenerNombre (obtenerLiteral c)) modelo
+                                  then acumularClausula ([], [c]) (unit (modelo , xs))
+                                  else (modelo ++ darValor c, xs)
+                         else acumularClausula ([], [c]) (unit (modelo , xs))
 
 --Ejercicio 4
 elim :: Estado -> Estado
@@ -72,3 +77,52 @@ dpll = undefined
 --EXTRA
 dpll2 :: Prop -> Interpretacion
 dpll2 = undefined
+
+--Funciones auxiliares para la practica
+esUnitaria :: Clausula -> Bool
+esUnitaria [x] = True
+esUnitaria xs = False
+
+obtenerNombre :: Literal -> String
+obtenerNombre (Var x) = x
+obtenerNombre (Not (Var x)) = x
+
+tieneInterpretacion :: String -> Interpretacion -> Bool
+tieneInterpretacion _ [] = False
+tieneInterpretacion x ((y,b):ys) = if x == y
+                                   then True
+                                   else tieneInterpretacion x ys
+
+obtenerLiteral :: Clausula -> Literal
+obtenerLiteral [x] = x
+obtenerLiteral xs = Var "foo"
+
+darValor :: Clausula -> Interpretacion
+darValor [Var p] = [("p", True)]
+darValor [Not (Var p)] = [("p", False)]
+
+acumularClausula :: Estado -> Estado -> Estado
+acumularClausula (_ , xs) (l2 , ys) = (l2, xs ++ ys)
+
+segundoElemto :: (a , b) -> b
+segundoElemto (_ , b) = b
+
+--construirArbolDPLL :: Estado -> ArbolDPLL
+--construirArbolDPLL estado
+--  | conflict estado = Node estado Void
+--  | success estado = Nodo estado void
+--  | segundoElemento propuesto /=  segundoElemento estado = Nodo estado (construirArbolDPLL propuesto)
+--  otherwise = Branch estado (construirArbolDPLL izq) (construirArbolDPLL der)
+--  where
+--    propuesto = red (elim(unit estado))
+--    literal = heuristicsLiteral (segundoElemento estado)
+--    (izq, der) = sep literal estado 
+
+explorarArbolDPLL :: ArbolDPLL -> Estado
+explorarArbolDPLL (Node estado Void) = estado
+explorarArbolDPLL (Node _ subarbol) = explorarArbolDPLL subarbol
+explorarArbolDPLL (Branch estado izq der) = if conflict x
+                                               then explorarArbolDPLL der
+                                               else x
+                                            where
+                                              x = explorarArbolDPLL izq
